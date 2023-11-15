0f3e0aca34e66716c28450ad4618de1c8b758778
Javier Moneo <jvr.moneo@gmail.com>
2023-11-15 17:30:09 +0100

    Creando el componente de nuevo presupuesto

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,17 @@
+export const NuevoPresupuesto = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+        <div className="campo">
+          <label htmlFor="">Definir Presupuesto</label>
+          <input
+            type="text"
+            className="nuevo-presupuesto"
+            placeholder="Añade tu presupuesto"
+          />
+          <input type="submit" value="Añadir" />
+        </div>
+      </form>
+    </div>
+  );
+};
