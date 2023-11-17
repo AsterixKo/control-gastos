610fcad3ca13552feef02ef03cec66f225467ad4
Javier Moneo <jvr.moneo@gmail.com>
2023-11-17 12:21:55 +0100

    Mostrando los gastos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 import { Modal } from "./components/Modal";
 import { generarId } from "./helpers";
+import { ListadoGastos } from "./components/ListadoGastos";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
@@ -23,6 +24,7 @@ function App() {
 
   const guardarGasto = (gasto) => {
     gasto.id = generarId();
+    gasto.fecha = Date.now();
     setGastos([...gastos, gasto]);
 
     setAnimarModal(false);
@@ -41,13 +43,18 @@ function App() {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto && (
-        <div className="nuevo-gasto">
-          <img
-            src={IconoNuevoGasto}
-            alt="icono nuevo gasto"
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos gastos={gastos} />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
       )}
 
       {modal && (
