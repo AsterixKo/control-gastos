388dae13623dae2b78166b75e9d54f90d3dc6da2
Javier Moneo <jvr.moneo@gmail.com>
2023-11-17 18:35:36 +0100

    Mostrando gastos filtrados

--- src/App.jsx
+++ src/App.jsx
@@ -115,6 +115,8 @@ function App() {
               gastos={gastos}
               setGastoEditar={setGastoEditar}
               eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
             />
           </main>
           <div className="nuevo-gasto">
